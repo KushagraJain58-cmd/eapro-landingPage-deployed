ec8f00634627272ce959f728489aa6ae38d920cc
Kushagra Jain <ishaan@Kushagras-MacBook-Air.local>
2025-09-19 00:16:30 +0530

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable rule globally
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
